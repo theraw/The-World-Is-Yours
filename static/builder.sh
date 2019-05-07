e62368f7d4c43bebac1693022af645e20e27a3a3
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-05-07 22:27:12 +0200

    Create builder.sh

---
+++ static/builder.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Please use this to build dynamic modules every single detail is strictly required!
+./configure                                       \
+--user=nginx                                      \
+--group=nginx                                     \
+--modules-path=/nginx/modules/                    \
+--sbin-path=/usr/sbin/nginx                       \
+--conf-path=/nginx/nginx.conf                     \
+--pid-path=/run/nginx.pid                         \
+--lock-path=/run/nginx.lock                       \
+--error-log-path=/var/log/nginx/error.log         \
+--http-log-path=/var/log/nginx/access.log         \
+--with-pcre                                       \
+--with-threads                                    \
+--with-file-aio                                   \
+--with-http_ssl_module                            \
+--with-http_v2_module                             \
+--with-http_realip_module                         \
+--with-http_addition_module                       \
+--with-http_xslt_module                           \
+--with-http_image_filter_module                   \
+--with-http_geoip_module                          \
+--with-http_sub_module                            \
+--with-http_dav_module                            \
+--with-http_flv_module                            \
+--with-http_mp4_module                            \
+--with-http_gunzip_module                         \
+--with-http_gzip_static_module                    \
+--with-http_auth_request_module                   \
+--with-http_random_index_module                   \
+--with-http_secure_link_module                    \
+--with-http_slice_module                          \
+--with-http_stub_status_module                    \
+--with-mail                                       \
+--with-mail_ssl_module                            \
+--with-stream                                     \
+--with-stream_ssl_module                          \
+--with-stream_realip_module                       \
+--with-stream_geoip_module                        \
+--with-ld-opt="-Wl,-rpath,/usr/local/lib/"        \
+--add-dynamic-module=/the path of /ngx_devel_kit \
+--add-dynamic-module=/your module path
