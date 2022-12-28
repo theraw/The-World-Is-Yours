da9a4fee6207ee8384cad3b1c6bda83a9eb0ef62
ƬHE ЯAW ☣ <daraw665@gmail.com>
2022-12-28 02:56:19 +0100

    Create Builder.sh

---
+++ static/Focal/Builder.sh
@@ -0,0 +1,38 @@
+./configure --with-compat \
+--user=nginx                                      \
+--group=nginx                                     \
+--sbin-path=/usr/sbin/nginx                       \
+--conf-path=/nginx/nginx.conf                     \
+--pid-path=/var/run/nginx.pid                     \
+--lock-path=/var/run/nginx.lock                   \
+--error-log-path=/var/log/nginx/error.log         \
+--http-log-path=/var/log/nginx/access.log         \
+--with-threads                                    \
+--with-file-aio                                   \
+--with-http_ssl_module                            \
+--with-http_v2_module                             \
+--with-http_realip_module                         \
+--with-http_addition_module                       \
+--with-http_xslt_module                           \
+--with-http_image_filter_module                   \
+--with-http_geoip_module                          \
+--with-http_sub_module                            \
+--with-http_dav_module                            \
+--with-http_flv_module                            \
+--with-http_mp4_module                            \
+--with-http_gunzip_module                         \
+--with-http_gzip_static_module                    \
+--with-http_auth_request_module                   \
+--with-http_random_index_module                   \
+--with-http_secure_link_module                    \
+--with-http_slice_module                          \
+--with-http_stub_status_module                    \
+--with-mail                                       \
+--with-mail_ssl_module                            \
+--with-stream                                     \
+--with-stream_ssl_module                          \
+--with-stream_realip_module                       \
+--with-stream_geoip_module                        \
+--with-http_v2_hpack_enc                          \
+--with-cc-opt="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC" \
+--with-ld-opt="-Wl,-rpath,/usr/local/lib -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie,-lpcre"
